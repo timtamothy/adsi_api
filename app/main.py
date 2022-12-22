2b9f5cf71af3c329b2973c83e9ddf51fbb0670f7
Tim Wang <stevetimothy@gmail.com>
2022-12-22 22:44:08 +1100

    Model 2 Pipeline
    Model now accepts string input for brewery name and outputs string of beer type

--- app/main.py
+++ app/main.py
@@ -3,22 +3,22 @@ from starlette.responses import JSONResponse
 from joblib import load
 import pandas as pd
 app = FastAPI()
-nn_pipe = load('../models/nn_pipe.joblib')
+nn_pipe2 = load('../models/models/nn_pipe2.joblib')
 
-@app.get("/")
+@app.get('/')
 def read_root():
-	return {"Hello": "AnyaChan"}
+	return {'Hello': 'This is Anya-Chan'}
 
-@app.get("/health", status_code=200)
+@app.get('/health', status_code=200)
 def healthcheck():
-	return "Anya is ready and very waku waku!"
+	return 'Anya is ready and very waku waku! Angus is ready and very wagyu wagyu'
 
-def format_features(brewery: str, review_aroma: int, review_appearance: int, review_palate: int, review_taste:int):
-	return{
-		'Brewery': [brewery],
+def format_features(brewery: str, review_aroma: int, review_appearance: int, review_palate: int, review_taste: int):
+	return {
+		'brewery_name': [brewery],
 		'Aroma': [review_aroma],
 		'Appearance': [review_appearance],
-		'Palate': [review_palate]
+		'Palate': [review_palate],
 		'Taste': [review_taste]
 	}
 
@@ -26,6 +26,6 @@ def format_features(brewery: str, review_aroma: int, review_appearance: int, rev
 def predict(brewery: str, review_aroma: int, review_appearance: int, review_palate: int, review_taste:int):
 	features = format_features(brewery, review_aroma, review_appearance, review_palate, review_taste)
 	obs = pd.DataFrame(features)
-	pred = nn_pipe.predict(obs)
+	pred = nn_pipe2.predict(obs)
 	return JSONResponse(pred.tolist())
 
